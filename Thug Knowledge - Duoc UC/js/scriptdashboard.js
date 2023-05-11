77b5b4d3fcadbb28ff5a609dfcb2a3f774944a9d
Francisco España <franespana48@gmail.com>
2023-05-11 18:01:53 -0400

    Inclusion de script y actualizacion dashboard

---
+++ Thug Knowledge - Duoc UC/js/scriptdashboard.js
@@ -0,0 +1,20 @@
+const listUsers = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+
+    let tableBody = ``;
+    users.forEach((user, index) => {
+        tableBody += `<tr>
+        <td class='centered'>${user.id}</td>
+        <td class='centered'>${user.name}</td>
+        <td class='centered'>${user.username}</td>
+        <td class='centered'>${user.email}</td>
+        </tr>`;
+    });
+    // document.getElementById("tableBody_Users").innerHTML = tableBody;
+    tablausuarios.innerHTML = tableBody;
+};
+
+window.addEventListener("load", function () {
+    listUsers();
+});
